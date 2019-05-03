b56421114841180351dfcaf5af14713c43079dd4
Nataliawas <nataliawas@wp.pl>
2019-05-03 15:53:43 +0200

    firebase

--- src/TodoTable/TodoTable.js
+++ src/TodoTable/TodoTable.js
@@ -2,22 +2,47 @@ import React from 'react'
 import { db } from '../config/firebase'
 
 class TodoTable extends React.Component {
-    state = {
-        tasks: []
-    }
+  state = {
+    tasks: []
+  }
 
-    componentDidMount() {
-        db.ref('tasks/').on("value", snapshot => {
-          console.log(snapshot.val())
+
+  componentDidMount() {
+    db.ref('tasks/').on("value", snapshot => {
+      const tasks = []
+      Object.entries(snapshot.val()).forEach(elem => {
+
+        const id = Object.keys(elem[1])[0];
+  
+        const task = {
+          id: id,
+          ...elem[1][id]
+        }
+        tasks.push(task)
+       
+        this.setState({ tasks })
+      
       })
-    }
+      
+    })
+  }
 
-render () {
+
+
+  render() {
     return (
-      <p>cycki</p>
+    <div>
+      {this.state.tasks.map(el=>
+      <div>
+        <p>{el.title}</p>
+        <p>{el.description}</p>
+        </div>
+      )}
+    </div>
+  
     )
 
-}
+  }
 }
 
 export default TodoTable
