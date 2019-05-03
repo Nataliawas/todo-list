9433fd68938051287c5e1f41667317e0ba5de2b3
Nataliawas <nataliawas@wp.pl>
2019-05-03 12:25:50 +0200

    initial

---
+++ src/TodoTable/TodoTable.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import { db } from '../config/firebase'
+
+class TodoTable extends React.Component {
+
+render () {
+    return (
+        <p>test</p>
+    )
+
+}
+}
+
+export default TodoTable
---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+// const config = {
+//     apiKey: "AIzaSyDpGiuPB8AzkDLDmJtMPaWcXLiJ3OwxGpo",
+//     authDomain: "todo-list-b01b8.firebaseapp.com",
+//     databaseURL: "https://todo-list-b01b8.firebaseio.com",
+//     projectId: "todo-list-b01b8",
+//     storageBucket: "todo-list-b01b8.appspot.com",
+//     messagingSenderId: "281981327349"
+// }
+
+// const app = firebase.initializeApp(config)
+
+// const db = app.database()
+
+// export default db
